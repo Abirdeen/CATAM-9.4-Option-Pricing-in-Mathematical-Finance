d8c200906c37fd893598ca701542a24ca23d6c58
Abirdeen <102402217+Abirdeen@users.noreply.github.com>
2026-03-12 02:11:29 +0000

    docs: added docstring for black-scholes

--- src/asset.rs
+++ src/asset.rs
@@ -26,6 +26,16 @@ impl EuropeanCall {
         return EuropeanCall{strike_price, strike_time, underlying}
     }
 
+    /** Computes the Black-Scholes price.
+
+        Parameters
+        ----------
+        * `interest_rate` : The continuously compounded risk-free interest rate.
+
+        Returns
+        -------
+        * `f64` : The price of the call.
+    */
     pub fn black_scholes_price(&self, interest_rate: f64) -> Result<f64, rs_stats::StatsError> {
         return black_scholes_price(self, interest_rate)
     }
