c20a44828cef7cad6553f5e9a002ee165e136ffb
Abirdeen <102402217+Abirdeen@users.noreply.github.com>
2026-03-12 02:01:25 +0000

    refactor: change asset pricing info
    Previously, the forward price was a constant
    associated with the underlying. This is wrong: the
    forward price should be a function of spot price
    and time.

--- src/asset_types.rs
+++ src/asset_types.rs
@@ -1,10 +1,26 @@
 pub struct Underlying {
-    pub fwd_price: f64,
+    pub spot_price: f64,
     pub std_dev: f64
+}
+
+impl Underlying {
+    pub fn new(spot_price: f64, std_dev: f64) -> Underlying {
+        return Underlying{spot_price, std_dev}
+    }
+
+    pub fn fwd_price(&self, time: f64, interest: f64) -> f64 {
+        return self.spot_price*(interest*time).exp()
+    }
 }
 
 pub struct EuropeanCall {
     pub strike_price: f64,
     pub strike_time: f64,
     pub underlying: Underlying
+}
+
+impl EuropeanCall {
+    pub fn new(strike_price: f64, strike_time: f64, underlying: Underlying) -> EuropeanCall {
+        return EuropeanCall{strike_price, strike_time, underlying}
+    }
 }
--- src/black_scholes.rs
+++ src/black_scholes.rs
@@ -33,7 +33,8 @@ fn discount_factor(strike_time: f64, interest_rate: f64) -> f64 {
 */
 fn stock_moneyness(call: &EuropeanCall, interest_rate: f64) -> f64 {
     let underlying = &call.underlying;
-    let moneyness = 1.0/(underlying.std_dev*call.strike_time.sqrt()) * ((underlying.fwd_price/call.strike_price).ln() + (interest_rate + underlying.std_dev.powf(2.0)/2.0)*call.strike_time);
+    let fwd_price = underlying.fwd_price(call.strike_time, interest_rate);
+    let moneyness = 1.0/(underlying.std_dev*call.strike_time.sqrt()) * ((fwd_price/call.strike_price).ln() + (interest_rate + underlying.std_dev.powf(2.0)/2.0)*call.strike_time);
     return moneyness
 }
 
@@ -68,9 +69,10 @@ fn rfa_moneyness(call: &EuropeanCall, interest_rate: f64) -> f64 {
 pub fn black_scholes_price(call: &EuropeanCall, interest_rate: f64) -> Result<f64,StatsError> {
     let std_norm_cdf = |x: f64| normal_cdf(x, 0.0, 1.0);
     let underlying = &call.underlying;
+    let fwd_price = underlying.fwd_price(call.strike_time, interest_rate);
 
     let discount = discount_factor(call.strike_time, interest_rate);
-    let undiscounted_price = std_norm_cdf(stock_moneyness(call, interest_rate))?*underlying.fwd_price - std_norm_cdf(rfa_moneyness(call, interest_rate))?*call.strike_price;
+    let undiscounted_price = std_norm_cdf(stock_moneyness(call, interest_rate))?*fwd_price - std_norm_cdf(rfa_moneyness(call, interest_rate))?*call.strike_price;
 
     let price = discount * undiscounted_price;
     return Ok(price)
