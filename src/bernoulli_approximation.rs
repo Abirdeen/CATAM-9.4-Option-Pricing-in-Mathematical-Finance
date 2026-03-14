0a5b962dbc986ddc1b3442fb30fca418d1522f3e
Abirdeen <102402217+Abirdeen@users.noreply.github.com>
2026-03-14 03:17:22 +0000

    fix: correct bernoulli algorithm

--- src/bernoulli_approximation.rs
+++ src/bernoulli_approximation.rs
@@ -7,12 +7,11 @@ pub trait BernoulliApproxable: CallOption {
         step_prices: &Vec<Vec<f64>>) -> f64;
 
     fn approximate_price(&self, steps: usize) -> f64 {
-        let mut step_prices = vec![vec![f64::NAN;steps];steps];
-        for i in 0..steps {
-            for j in 0..i {
+        let mut step_prices = vec![vec![f64::NAN;steps + 1];steps + 1];
+        for i in 0..=steps {
+            for j in 0..=(steps - i) {
                 step_prices[steps - i][j] = self.step_price(steps - i, j, steps, &step_prices)
             }
-
         }
         return step_prices[0][0]
     }
@@ -28,7 +27,7 @@ impl BernoulliApproxable for EuropeanCall {
             let spot_price = underlying.spot_price;
             let strike_price = self.strike_price;
             let g=underlying.std_dev * step_time.sqrt();
-            return (spot_price * ((2*j - steps) as f64 * g).exp() - strike_price).max(0.0)
+            return (spot_price * (((2*j) as f64 - (steps as f64)) * g).exp() - strike_price).max(0.0)
         } else {
             let p = 0.5 + 0.5*(underlying.mean()/underlying.std_dev)*step_time.sqrt();
             let discount = underlying.discount_factor(step_time);
